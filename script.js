65d1239548a80bd0efbe392bc8ac033cb24c4bac
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-12 18:19:55 +0000

    string

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 let number = 0
-let sentences = "hello"
-
 document.getElementById('c').addEventListener('click', multiplication)
 
-// This code gives me a calculater that calculates area of squars
+// This code gives me a calculator  that calculates area of squars
 function multiplication () {
   number = document.getElementById('input').value
-
   number = parseInt(number)
-
   number = number * number
-
   alert(number)
 }
